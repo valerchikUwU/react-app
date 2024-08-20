14eefdbcb540a829624859604eafd3ac98ae540b
viyaya <johndoe@email.com>
2024-08-20 16:34:57 +0300

    Worker

--- public/worker.js
+++ public/worker.js
@@ -1,81 +1,62 @@
-// // Filename - public/worker.js
+let STATIC_CACHE_NAME = "gfg-pwa";
+let DYNAMIC_CACHE_NAME = "dynamic-gfg-pwa";
 
-// let STATIC_CACHE_NAME = "gfg-pwa";
-// let DYNAMIC_CACHE_NAME = "dynamic-gfg-pwa";
+let urlsToCache = 
+[
+    '#/',
+    '#/:accountId/user',
+    '#/:accountId/user/new',
+    '#/:accountId/user/new/start',
+    '#/:accountId/user/new/basic',
+    '#/:accountId/user/new/personal',
+    '#/:accountId/user/new/deposit',
+    '#/:accountId/user/work',
+    '#/:accountId/user/completed',
+];
 
-// // Add Routes and pages using React Browser Router
-// let urlsToCache = 
-// [
-//     '#/',
-//     '#/:accountId/user',
-//     '#/:accountId/user/new',
-//     '#/:accountId/user/new/start',
-//     '#/:accountId/user/new/basic',
-//     '#/:accountId/user/new/personal',
-//     '#/:accountId/user/new/deposit',
-//     '#/:accountId/user/work',
-//     '#/:accountId/user/completed',
-// ];
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(STATIC_CACHE_NAME).then(function(cache) {
+            console.log("Opened cache");
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
 
-// // Install a service worker
-// self.addEventListener("install", (event) => {
-//     // Perform install steps
-//     event.waitUntil(
-//         caches
-//             .open(STATIC_CACHE_NAME)
-//             .then(function (cache) {
-//                 console.log("Opened cache");
-//                 return cache.addAll(urlsToCache);
-//             })
-//     );
-// });
+self.addEventListener("activate", (event) => {
+    let cacheWhitelist = ["gfg-pwa"];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if (cacheWhitelist.indexOf(cacheName) === -1) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+    );
+});
 
-// // Cache and return requests
-// self.addEventListener("fetch", (event) => {
-//     // Проверяем, есть ли соединение
-//     navigator.onLine? fetchFromNetwork(event) : fetchFromCache(event);
+self.addEventListener("fetch", (event) => {
+    if (!navigator.onLine) {
+        event.respondWith(fromCache(event.request));
+        return;
+    }
 
-//     // Функция для получения ресурса из сети
-//     function fetchFromNetwork(event) {
-//         return fetch(event.request).then((fetchRes) => {
-//             // Если ресурс получен успешно, сохраняем его в динамический кэш
-//             caches.open(DYNAMIC_CACHE_NAME).then((cache) => {
-//                 cache.put(event.request.url, fetchRes.clone());
-//             });
-//             // Возвращаем результат из сети
-//             return fetchRes;
-//         }).catch((error) => {
-//             // В случае ошибки в сети, пытаемся получить ресурс из кэша
-//             return caches.match(event.request);
-//         });
-//     }
+    event.respondWith(fromNetwork(event.request).catch(() => fromCache(event.request)));
+});
 
-//     // Функция для получения ресурса из кэша
-//     function fetchFromCache(event) {
-//         return caches.match(event.request).then((cacheRes) => {
-//             // Если ресурс найден в кэше, возвращаем его
-//             return cacheRes || fetch(event.request);
-//         });
-//     }
-// });
+async function fromNetwork(request) {
+    const response = await fetch(request);
+    const cache = await caches.open(DYNAMIC_CACHE_NAME);
 
+    cache.put(request, response.clone());
 
-// // Update a service worker
-// self.addEventListener("activate", (event) => {
-//     let cacheWhitelist = ["gfg-pwa"];
-//     event.waitUntil(
-//         caches.keys().then((cacheNames) => {
-//             return Promise.all(
-//                 cacheNames.map((cacheName) => {
-//                     if (
-//                         cacheWhitelist.indexOf(
-//                             cacheName
-//                         ) === -1
-//                     ) {
-//                         return caches.delete(cacheName);
-//                     }
-//                 })
-//             );
-//         })
-//     );
-// });
+    return response;
+}
+
+async function fromCache(request) {
+    const cache = await caches.open(DYNAMIC_CACHE_NAME);
+    const response = await cache.match(request);
+
+    return response || Promise.reject('No internet connection found');
+};
