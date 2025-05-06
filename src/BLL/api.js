c2def8a0920a937fa8538627aeb511aa49319ce6
viyaya <johndoe@email.com>
2025-05-06 17:03:12 +0300

    baseURL

--- src/BLL/api.js
+++ src/BLL/api.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/api/',
+  // baseURL: 'http://localhost:3000/api/',
+  baseURL: process.env.REACT_APP_BASE_URL_INSTANCE,
   headers: {
     'Content-Type': 'application/json',
  },
